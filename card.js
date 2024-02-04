09e52e6ebe88fe9c8287a87e78825944e8988f2a
Chelsea Dwarika <chelseakristina@live.com>
2024-02-04 14:53:33 -0500

    adding ids to cards for dropdown menu

--- card.js
+++ card.js
@@ -1,7 +1,7 @@
 const cardTemplate = (card) => `
 <div class="card-inner">
   <div class="card-front">
-    <img src="${card.img_url}" alt="Card 1">
+    <img src="${card.img_url}" alt=${card.title}>
   </div>
   <div class="card-back">
     <h2 class="title1">${card.title}</h2>
@@ -11,29 +11,24 @@ const cardTemplate = (card) => `
 
 document.addEventListener("DOMContentLoaded", function () {
   const cardData = [
-//    { title: "The Empress", description: "Chris", img_url: "https://cdn.pixabay.com/photo/2016/03/01/12/43/lines-1230292_1280.png",},
-//    { title: "The Fool", description: "Chris" },
-//    { title: "The Chariot", description: "Chelsea" },
+    //    { title: "The Empress", description: "Chris", img_url: "https://cdn.pixabay.com/photo/2016/03/01/12/43/lines-1230292_1280.png",},
+    //    { title: "The Fool", description: "Chris" },
+    //    { title: "The Chariot", description: "Chelsea" },
   ];
 
   function renderCardData() {
-    const list = document.querySelector("#card-list");
-    cardData.forEach((data, index) => {
-      const card = document.createElement("div");
-      card.classList.add("tarot-card");
-      card.innerHTML = cardTemplate(data);
-      list.appendChild(card);
-    });
+      const list = document.querySelector("#card-list");
 
     fetch("/cards/cards.txt")
       .then((res) => res.text())
       .then((text) => {
         const cards = text.split("---").map((card) => card.trim());
         cards.forEach((data) => {
-          const [title, img_url, ...description] = data.split("\n");
+          const [id, title, img_url, ...description] = data.split("\n");
 
           const card = document.createElement("div");
           card.classList.add("tarot-card");
+          card.id = id;
           card.innerHTML = cardTemplate({
             title,
             img_url,
